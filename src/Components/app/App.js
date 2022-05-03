e59b8aefbe262819f2b784d5e6c38a06469b5138
LoganW <logan.willaumez@gmail.com>
2022-05-03 15:51:25 +0200

    feat(route): add final routes
    add dashboard route, test it, all ok

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import { Homepage } from '../Homepage/Homepage';
+import { Dashboard } from '../Dashboard/Dashboard';
 import './App.scss';
 // import { Counter } from '../features/counter/Counter';
 
@@ -10,8 +11,8 @@ const App = () => {
       <h1>Coucou</h1>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-      {/* <Counter /> */}
     </div>
   );
 };
