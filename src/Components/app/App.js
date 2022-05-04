dc72bc2e2998778a8a4a9675c6a42f37968e9bdd
LoganW <logan.willaumez@gmail.com>
2022-05-04 20:31:52 +0200

    feat(darkmode): add dark mode and style connected to it.

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -3,17 +3,31 @@ import { Routes, Route } from 'react-router';
 import { Homepage } from '../Homepage/Homepage';
 import { Dashboard } from '../Dashboard/Dashboard';
 import './App.scss';
+import { DarkMode } from '../DarkMode/DarkMode';
 // import { Counter } from '../features/counter/Counter';
+import useLocalStorage from 'use-local-storage';
 
+// Detect the prefer color scheme from the user, and add it automatically to the local storage.
+const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const App = () => {
+  // Allow automatically the theme value based on the prefer color scheme from the user.
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  );
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
   return (
-    <div className='App'>
+    <div className='App' data-theme={theme}>
       <h1>Coucou</h1>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/dashboard' element={<Dashboard />} />
         {/* <Route path='*' element={<Error404 />} /> */}
       </Routes>
+      <DarkMode switchTheme={switchTheme} />
     </div>
   );
 };
