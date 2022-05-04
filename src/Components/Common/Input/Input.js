258ab61de3be6c9b74346280fa1ae49b122ec30c
LoganW <logan.willaumez@gmail.com>
2022-05-04 14:34:19 +0200

    feat(form): state OK with login form

---
+++ src/Components/Common/Input/Input.js
@@ -0,0 +1,32 @@
+import TextField from '@mui/material/TextField';
+import { useDispatch } from 'react-redux';
+import { handleChange } from '../../../features/auth/authSlice';
+import './Input.scss';
+
+export const Input = ({ type, name }) => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <TextField
+        id='standard-basic'
+        label={name}
+        variant='standard'
+        name={name}
+        placeholder={name}
+        required
+        type={type}
+        /* On récupère le nom du login (que ce soit donc e-mail ou password)
+        Quand on va modifier un champ texte , il récupère automatiquement le nom (email ou password en fonction sur lequel on est) et la valeur. On utilise donc un dispatch pour utiliser une des fonctions de changement du state (ici handleChange) qui va changer le champ que l'on veut modifier et sa valeur automatiquement.
+        */
+        onChange={(event) =>
+          dispatch(
+            handleChange({
+              name,
+              payload: event.target.value,
+            })
+          )
+        }
+      />
+    </>
+  );
+};
