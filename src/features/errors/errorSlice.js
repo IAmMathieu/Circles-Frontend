c54a0c3ff0b6e8ecdcadd082db8dd5138d9bda33
LoganW <logan.willaumez@gmail.com>
2022-05-05 10:27:18 +0200

    feat(errors) add foler and file for error catch

---
+++ src/features/errors/errorSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const errorsSlice = createSlice({
+  name: 'errors',
+  initialState: '',
+  reducers: {},
+});
+
+export default errorsSlice.reducer;
