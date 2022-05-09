fb9d21fb929843ece72388ddfb75d4425939f14f
ElodieBustin <elodie.bustin@lilo.org>
2022-05-09 13:58:59 +0200

    adjust css

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { emptySplitApi } from '../features/api/emptySplitApi';
 =======
 import { applyMiddleware, compose } from 'redux';
 >>>>>>> feat(circle): attempt connect chatroom
+=======
+>>>>>>> adjust css
 import authReducer from '../features/auth/authSlice';
-import socketMW from './../features/CirclePage/Chatroom/middlewares';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancers = composeEnhancers(applyMiddleware(socketMW));
 
 export const store = configureStore({
   reducer: {
@@ -16,7 +15,6 @@ export const store = configureStore({
     // [authApi.reducerPath]: authApi.reducer,
     [emptySplitApi.reducerPath]: emptySplitApi.reducer,
     auth: authReducer,
-    enhancers,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
