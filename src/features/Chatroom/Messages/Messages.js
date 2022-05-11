9a4ac26fe5e2f8c4bde18faa4d93e790b1b8ca69
ElodieBustin <elodie.bustin@lilo.org>
2022-05-11 14:36:38 +0200

    feat(circle): action-type tchat

--- src/features/Chatroom/Messages/Messages.js
+++ src/features/Chatroom/Messages/Messages.js
@@ -1,8 +1,9 @@
 import './Messages.scss';
 import Message from '../Message/Message';
+import { useSelector } from 'react-redux';
 
 export default function Messages() {
-  
+  const { messages } = useSelector((state) => state.tchat);
   return (
     <ul className="messages">
       {messages.map((message)=> (
