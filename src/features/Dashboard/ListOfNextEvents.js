870edf2923f146dd225ece3fd1204c4b79baa629
LoganW <logan.willaumez@gmail.com>
2022-05-18 16:43:53 +0200

    [FIX](fix-event-number): add fix for the next evenets

--- src/features/Dashboard/ListOfNextEvents.js
+++ src/features/Dashboard/ListOfNextEvents.js
@@ -4,13 +4,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { Box, Grid, Modal, Slide, Typography } from '@mui/material';
 import { format } from 'date-fns';
 import './style.scss';
+import moment from 'moment';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: {md: '50%', xs: '80%'},
+  width: { md: '50%', xs: '80%' },
   textAlign: 'center',
   bgcolor: 'background.paper',
   border: '2px solid #000',
@@ -20,6 +21,11 @@ const style = {
 };
 
 export default function ListOfNextEvents({ open, onClose, eventsList }) {
+  const arrayToSort = [...eventsList];
+  const newArray = arrayToSort?.sort((a, b) =>
+    moment(a?.start, 'DD-MM-YYYY').diff(moment(b?.start, 'DD-MM-YYYY'))
+  );
+  // console.log(`🚀 ~ sortEvenList`, sortEvenList);
   return (
     <div>
       <Modal open={open} onClose={onClose}>
@@ -35,20 +41,32 @@ export default function ListOfNextEvents({ open, onClose, eventsList }) {
           >
             Prochains événements
           </Typography>
-          <Typography>
-            {eventsList.map((event) => {
-              if (event !== null) {
+          <Box>
+            {newArray?.map((event, i) => {
+              const now = moment().format('YYYY-MM-DD');
+              const eventToCheck = moment(event?.start).format('YYYY-MM-DD');
+              const isValid = eventToCheck > now ? event : null;
+              if (isValid !== null) {
                 return (
-                  <Grid container key={Date.now()}>
-                    <Grid item xs={12} sm={4} sx={{fontSize:'.8rem', alignSelf: 'center', borderRight: {sm:'1px solid #000'}}}>
-                      {format(new Date(event.start), 'dd/MM/yyyy')}
-                    </Grid>
-                    <Grid item xs={12} sm={8} >{event.title}</Grid>
+                  <Grid container key={i + Date.now()}>
+                    <Typography
+                      sx={{
+                        fontSize: '.8rem',
+                        alignSelf: 'center',
+                        borderRight: { sm: '1px solid #000' },
+                        width: '100px',
+                      }}
+                    >
+                      {format(new Date(isValid.start), 'dd/MM/yyyy')}
+                    </Typography>
+                    <Typography sx={{ marginLeft: '15px' }}>
+                      {isValid.title}
+                    </Typography>
                   </Grid>
                 );
               }
             })}
-          </Typography>
+          </Box>
         </Box>
       </Modal>
     </div>
