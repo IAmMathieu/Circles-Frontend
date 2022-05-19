6eb1ae5bdc7191574189cf99a70b3dce0d593bd3
alexandrecrevel <alex.crevel@gmail.com>
2022-05-19 20:33:48 +0200

    fix(background): fix background color on contact page and profile page

---
+++ src/features/Dashboard/DashbordLoader 2.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function DashbordLoader(props) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant='determinate' {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant='body2' color='text.secondary'>{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+DashbordLoader.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
+
+export default function LinearWithValueLabel({ text, time }) {
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, time * 100);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        width: '80%',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <Typography>{text}</Typography>
+      <DashbordLoader value={progress} />
+    </Box>
+  );
+}
---
+++ src/features/Dashboard/ListOfNextEvents 2.js
@@ -0,0 +1,73 @@
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Box, Grid, Modal, Slide, Typography } from '@mui/material';
+import { format } from 'date-fns';
+import './style.scss';
+import moment from 'moment';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: { md: '50%', xs: '80%' },
+  textAlign: 'center',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  borderRadius: '20px',
+  p: 4,
+};
+
+export default function ListOfNextEvents({ open, onClose, eventsList }) {
+  const arrayToSort = [...eventsList];
+  const newArray = arrayToSort?.sort((a, b) =>
+    moment(a?.start, 'DD-MM-YYYY').diff(moment(b?.start, 'DD-MM-YYYY'))
+  );
+  return (
+    <div>
+      <Modal open={open} onClose={onClose}>
+        <Box sx={style} className='modal'>
+          <Typography
+            variant='h3'
+            sx={{
+              fontWeight: 'bold',
+              fontSize: '1.5rem',
+              marginBottom: '.8rem',
+              textDecoration: 'underline',
+            }}
+          >
+            Prochains événements
+          </Typography>
+          <Box>
+            {newArray?.map((event, i) => {
+              const now = moment().format('YYYY-MM-DD');
+              const eventToCheck = moment(event?.start).format('YYYY-MM-DD');
+              const isValid = eventToCheck > now ? event : null;
+              if (isValid !== null) {
+                return (
+                  <Grid container key={i + Date.now()}>
+                    <Typography
+                      sx={{
+                        fontSize: '.8rem',
+                        alignSelf: 'center',
+                        borderRight: { sm: '1px solid #000' },
+                        width: '100px',
+                      }}
+                    >
+                      {format(new Date(isValid.start), 'dd/MM/yyyy')}
+                    </Typography>
+                    <Typography sx={{ marginLeft: '15px' }}>
+                      {isValid.title}
+                    </Typography>
+                  </Grid>
+                );
+              }
+            })}
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
