e8ff4d06aafe26758adf2bde2493fb403077e9bb
alexandrecrevel <alex.crevel@gmail.com>
2022-05-20 10:32:34 +0200

    fix(botnav): fix botnav to use bottomnavigateaction and fix showlabel error

--- src/features/Common/BottomNavigation/SimpleBottomNavigation.js
+++ src/features/Common/BottomNavigation/SimpleBottomNavigation.js
@@ -3,7 +3,7 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import GroupsIcon from '@mui/icons-material/Groups';
 import SearchIcon from '@mui/icons-material/Search';
-import { Typography } from '@mui/material';
+import { BottomNavigationAction, Typography } from '@mui/material';
 import './SimpleBottomNavigation.scss';
 export default function SimpleBottomNavigation({
   handleClickOpenCreate,
@@ -26,38 +26,30 @@ export default function SimpleBottomNavigation({
         },
       }}
     >
-      <BottomNavigation className='h-full px-10'>
-        <div className=' w-full flex justify-between items-center'>
-          <Box
-            className='flex flex-col items-center hover:cursor-pointer'
+      <BottomNavigation className='h-full px-10' showLabels>
+
+          <BottomNavigationAction
+            label='Créer'
+            icon={<AddCircleIcon />}
             onClick={() => {
               handleClickOpenCreate();
             }}
-          >
-            <AddCircleIcon />
-            {/* <Typography className='text-xs custom-bk:text-2xl' component='h5'> */}
-            <Typography className='text-xs custom-bk:text-lg' component='h5'>
-              Créer
-            </Typography>
-          </Box>
-          <Box
-            className='flex flex-col items-center hover:cursor-pointer'
+          />
+          <BottomNavigationAction
+            label='Rejoindre'
+            icon={<GroupsIcon />}
             onClick={() => {
               handleClickOpenJoin();
             }}
-          >
-            <GroupsIcon />
-            <Typography className='text-xs custom-bk:text-lg' component='h5'>
-              Rejoindre
-            </Typography>
-          </Box>
-          <div className='flex flex-col items-center hover:cursor-pointer'>
-            <SearchIcon />
-            <Typography className='text-xs custom-bk:text-lg' component='h5'>
-              Découvrir
-            </Typography>
-          </div>
-        </div>
+          />
+           <BottomNavigationAction
+            label='Découvrir'
+            icon={<SearchIcon />}
+            onClick={() => {
+              handleClickOpenJoin();
+            }}
+          />
+         
       </BottomNavigation>
     </Box>
   );
